813920f73d3e44aba7dcdddef2db36bb0e4c882e
CraciunIonut <alexionutcraciun@gmail.com>
2022-06-01 21:34:46 +0300

    forgot to add

--- app.py
+++ app.py
@@ -3,6 +3,9 @@ from flask_restful import Api
 
 from getMethod import blueprintGet
 from postMethod import blueprintPost
+from mainMethod import blueprintMain
+from externalMethod import blueprintExternal
+
 from createdatabase import createdb
 
 app = Flask(__name__)
@@ -13,8 +16,9 @@ api_rest_port = 5001
 
 createdb()
 
-app.register_blueprint(blueprintGet, url_prefix='/GET')
-app.register_blueprint(blueprintPost, url_prefix='/POST')
-
+app.register_blueprint(blueprintMain, url_prefix='/')
+app.register_blueprint(blueprintGet, url_prefix='/Find')
+app.register_blueprint(blueprintPost, url_prefix='/Register')
+app.register_blueprint(blueprintExternal, url_prefix='/External')
 
 app.run(host=api_rest_host, port=api_rest_port, debug=True)
